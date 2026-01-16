89d9bb06a441d364ac3b2dd534dc7fc3a65f4faf
bephemeral <244405225+bephemeral@users.noreply.github.com>
2026-01-16 16:22:07 -0500

    moving camera

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 inline constexpr unsigned int SCR_WIDTH{ 800 };
 inline constexpr unsigned int SCR_HEIGHT{ 600 };
 
-inline constexpr float vertices[]{
+inline constexpr float CUBE_VERTICES[]{
     -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,  // bottom back left
      0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,  // bottom back right
      0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,  // top back right
@@ -58,7 +58,7 @@ inline constexpr float vertices[]{
     -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f
 };
-inline constexpr glm::vec3 cubePositions[]{
+inline constexpr glm::vec3 CUBE_POSITIONS[]{
     glm::vec3( 0.0f,  0.0f,  0.0f),
     glm::vec3( 2.0f,  5.0f, -15.0f),
     glm::vec3(-1.5f, -2.2f, -2.5f),
@@ -70,15 +70,43 @@ inline constexpr glm::vec3 cubePositions[]{
     glm::vec3( 1.5f,  0.2f, -1.5f),
     glm::vec3(-1.3f,  1.0f, -1.5f)
 };
-inline constexpr glm::vec3 rotAxis(1.0f, 0.3f, 0.5f);
+inline constexpr glm::vec3 CUBE_ROTATION_AXIS{ 1.0f, 0.3f, 0.5f };
+
+inline constexpr glm::vec3 CAMERA_FRONT{ 0.0f, 0.0f, -1.0f };
+inline constexpr glm::vec3 CAMERA_UP{ 0.0f, 1.0f,  0.0f };
+inline constexpr float CAMERA_SPEED{ 5.0f };
 
 void framebuffer_size_callback(GLFWwindow* window, const int width, const int height) {
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+float getDeltaTime() {
+    static float lastFrame{ 0.0f };
+
+    const float currentFrame{ static_cast<float>(glfwGetTime()) };  
+    const float deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+    return deltaTime;
+}
+
+glm::vec3 processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    const float speed{ CAMERA_SPEED * getDeltaTime() };
+    glm::vec3 change{ 0.0f, 0.0f, 0.0f };
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        change += speed * CAMERA_FRONT;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        change -= speed * CAMERA_FRONT;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        change -= glm::normalize(glm::cross(CAMERA_FRONT, CAMERA_UP)) * speed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        change += glm::normalize(glm::cross(CAMERA_FRONT, CAMERA_UP)) * speed;
+
+    return change;
 }
 
 int main() {
@@ -119,7 +147,7 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     // copy vertices into VBO
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(CUBE_VERTICES), CUBE_VERTICES, GL_STATIC_DRAW);
 
     // VBO VS VAO
     // VBO - move vertices to gpu once, reuse every frame
@@ -138,7 +166,7 @@ int main() {
     glBindVertexArray(VAO);
     // 2. copy our vertices array in a buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(CUBE_VERTICES), CUBE_VERTICES, GL_STATIC_DRAW);
     // 3. then set our vertex attributes pointers
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -150,8 +178,10 @@ int main() {
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
+    glm::vec3 cameraPos{ glm::vec3(0.0f, 0.0f, 3.0f) };
+
     while(!glfwWindowShouldClose(window)) {
-        processInput(window);
+        cameraPos += processInput(window);
 
         // clear depth and colour buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -161,22 +191,11 @@ int main() {
         container.bind();
         awesomeface.bind();
 
-        // camera
-        constexpr glm::vec3 cameraPos{ glm::vec3(0.0f, 0.0f, 3.0f) };
-        constexpr glm::vec3 cameraTarget{ glm::vec3(0.0f, 0.0f, 0.0f) };
-        glm::vec3 cameraDirection{ glm::normalize(cameraPos - cameraTarget) };
-        constexpr glm::vec3 up{ glm::vec3(0.0f, 1.0f, 0.0f) };
-        glm::vec3 cameraRight{ glm::normalize(glm::cross(up, cameraDirection)) };
-        glm::vec3 cameraUp{ glm::cross(cameraDirection, cameraRight) };
-
         // viewmatrix
         glm::mat4 view{ glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), 
   		                            glm::vec3(0.0f, 0.0f, 0.0f), 
   		                            glm::vec3(0.0f, 1.0f, 0.0f)) };
-        constexpr float radius = 10.0f;
-        const float camX = sin(glfwGetTime()) * radius;
-        const float camZ = cos(glfwGetTime()) * radius;
-        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+        view = glm::lookAt(cameraPos, cameraPos + CAMERA_FRONT, CAMERA_UP);
 
         // projection
         const glm::mat4 projection{ glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f) };
@@ -187,13 +206,13 @@ int main() {
         
         // draw boxes
         const float time = static_cast<float>(glfwGetTime());
-        for (const auto& [i, pos] : std::views::enumerate(cubePositions))
+        for (const auto& [i, pos] : std::views::enumerate(CUBE_POSITIONS))
         {
             // calculate the model matrix for each object and pass it to shader before drawing
             glm::mat4 model{ glm::mat4(1.0f) };
             model = glm::translate(model, pos);
             const float angle{ 20.0f * (i + 1) };
-            model = glm::rotate(model, time * glm::radians(angle), rotAxis);
+            model = glm::rotate(model, time * glm::radians(angle), CUBE_ROTATION_AXIS);
             ourShader.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
