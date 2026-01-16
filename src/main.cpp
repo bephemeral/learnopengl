3a65a2dad92a51a53c33b429b04c83a2d7cd8e2c
bephemeral <244405225+bephemeral@users.noreply.github.com>
2026-01-16 15:26:34 -0500

    many spinny cube

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
-#include <math.h>
+#include <ranges>
 #include "shader.h"
 #include "texture.h"
 
@@ -15,7 +15,7 @@
 inline constexpr unsigned int SCR_WIDTH{ 800 };
 inline constexpr unsigned int SCR_HEIGHT{ 600 };
 
-inline constexpr float vertices[] = {
+inline constexpr float vertices[]{
     -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,  // bottom back left
      0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,  // bottom back right
      0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,  // top back right
@@ -58,6 +58,19 @@ inline constexpr float vertices[] = {
     -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f
 };
+inline constexpr glm::vec3 cubePositions[]{
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+inline constexpr glm::vec3 rotAxis(1.0f, 0.3f, 0.5f);
 
 void framebuffer_size_callback(GLFWwindow* window, const int width, const int height) {
     glViewport(0, 0, width, height);
@@ -149,20 +162,28 @@ int main() {
         awesomeface.bind();
 
         // matrices
-        glm::mat4 model{ glm::mat4(1.0f) };
-        model = glm::rotate(model, static_cast<float>(glfwGetTime()) * glm::radians(50.0f), glm::vec3(1.0f, 1.0f, 1.0f));
         glm::mat4 view{ glm::mat4(1.0f) };
         // note that we're translating the scene in the reverse direction of where we want to move
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         glm::mat4 projection{ glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f) };
 
         // apply matrices
-        ourShader.setMat4("model", model);
         ourShader.setMat4("view", view);
         ourShader.setMat4("projection", projection);
+        
+        // draw boxes
+        float time = static_cast<float>(glfwGetTime());
+        for (const auto& [i, pos] : std::views::enumerate(cubePositions))
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model{ glm::mat4(1.0f) };
+            model = glm::translate(model, pos);
+            float angle{ 20.0f * (i + 1) };
+            model = glm::rotate(model, time * glm::radians(angle), rotAxis);
+            ourShader.setMat4("model", model);
 
-        // draw box
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();    
