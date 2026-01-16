f1af9db2a7db01f519f2641d853ead05370f27a4
bephemeral <244405225+bephemeral@users.noreply.github.com>
2026-01-16 12:24:38 -0500

    initial commit

---
+++ assets/shaders/shader.vs
@@ -0,0 +1,15 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColour;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec3 ourColour;
+out vec2 TexCoord;
+uniform float shift;
+
+void main()
+{
+    gl_Position = vec4(aPos.x + shift, -aPos.y, aPos.z, 1.0); // just add a - to the y position
+    ourColour = aColour;
+    TexCoord = aTexCoord;
+}
---
+++ src/main.cpp
@@ -0,0 +1,133 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <math.h>
+#include "shader.h"
+#include "texture.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+constexpr unsigned int SCR_WIDTH{ 800 };
+constexpr unsigned int SCR_HEIGHT{ 600 };
+
+constexpr float vertices[] = {
+    // positions          // colour          // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  1.0f, 1.0f,   // top right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  0.0f, 1.0f    // top left 
+};
+constexpr unsigned int indices[]{  // note that we start from 0!
+    0, 1, 3,
+    1, 2, 3
+};
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+    // init/config glfw
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // create window
+    GLFWwindow* window{ glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr) };
+    if (window == nullptr) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // init glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
+    }
+
+    Shader ourShader("assets/shaders/shader.vs", "assets/shaders/shader.fs");
+
+    // register window resize callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // set background colour
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    // generate VBO and store buffer id
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // copy vertices into VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // VBO VS VAO
+    // VBO - move vertices to gpu once, reuse every frame
+    // VAO - how to interpret the VBO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // EBO - decides order to draw vertices in
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    Texture container("assets/textures/container.jpg", GL_TEXTURE0, GL_RGB);
+    Texture awesomeface("assets/textures/awesomeface.png", GL_TEXTURE1, GL_RGBA);
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
+
+    // Initialization code (done once, unless object frequently changes)
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAO);
+    // 2. copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 3. then set our vertex attributes pointers
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // colour attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    while(!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        // clear background to set colour
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // move triangles
+        ourShader.setFloat("shift", std::sin(glfwGetTime()) / 2);
+
+        // set texture
+        container.bind();
+        awesomeface.bind();
+
+        // the meat and potatoes
+        ourShader.use();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
