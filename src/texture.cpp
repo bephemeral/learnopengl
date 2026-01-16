090ab4ed5073f3f986d460ccacd73bba6c28f5cc
bephemeral <244405225+bephemeral@users.noreply.github.com>
2026-01-16 12:33:35 -0500

    separate texture into header/src

---
+++ src/texture.cpp
@@ -0,0 +1,30 @@
+#include "texture.h"
+#include <iostream>
+#include "stb_image.h"
+
+Texture::Texture(const char* texturePath, GLenum textureUnit, GLenum textureFormat) {
+    glActiveTexture(textureUnit);
+    glGenTextures(1, &ID);
+    glBindTexture(GL_TEXTURE_2D, ID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(texturePath, &width, &height, &nrChannels, 0);
+
+    if(data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, textureFormat, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture " << texturePath << '\n';
+    }
+
+    stbi_image_free(data);
+}
+
+void Texture::bind() {
+    glBindTexture(GL_TEXTURE_2D, ID);
+}
--- src/texture.h
+++ src/texture.h
@@ -1,40 +1,13 @@
 #pragma once
 
 #include <glad/glad.h>
-#include <iostream>
-#include "stb_image.h"
 
-class Texture
-{
+class Texture {
 public:
     unsigned int ID;
 
-    Texture(const char* texturePath, GLenum textureUnit, GLenum textureFormat)
-    {
-        glActiveTexture(textureUnit);
-        glGenTextures(1, &ID);
-        glBindTexture(GL_TEXTURE_2D, ID);
+    Texture(const char* texturePath, GLenum textureUnit, GLenum textureFormat);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        int width, height, nrChannels;
-        unsigned char *data = stbi_load(texturePath, &width, &height, &nrChannels, 0);
-
-        if(data) {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, textureFormat, GL_UNSIGNED_BYTE, data);
-            glGenerateMipmap(GL_TEXTURE_2D);
-        } else {
-            std::cout << "Failed to load texture " << texturePath << '\n';
-        }
-
-        stbi_image_free(data);
-    }
-
-    void bind() {
-        glBindTexture(GL_TEXTURE_2D, ID);
-    }
+    void bind();
 };
 
